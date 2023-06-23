c9884199fb32bf3e952c973c7cb29608552a3f5c
Gabriel Souza <imgabriel.souza071@gmail.com>
2023-06-22 21:45:21 -0300

    project initial.

---
+++ componentes/concluiTarefa.js
@@ -0,0 +1,29 @@
+    //criando mais um componente 
+    const botaoConclui = () => {
+        const botaoFinalizado = document.createElement('button');
+
+        botaoFinalizado.classList.add('check-button');
+
+        //responsável por atribuir o texto ao botão criado.
+        botaoFinalizado.innerText = 'Concluir';
+
+        botaoFinalizado.addEventListener('click', concluirTarefa)
+
+        return botaoFinalizado
+    }
+
+    const concluirTarefa = (evento) => {
+        //qual elemento que eu cliquei? Você sabe através do objeto evento como parâmetro
+
+        //ele descobre qual o alvo do evento através do target
+        const botaoConclui = evento.target;
+
+        //ele sobe um nó no elemento, ou seja, sobre um item, ou para o PAI do elemento
+        const tarefaCompleta = botaoConclui.parentElement;
+        
+        // o método toggle vai executar a classe done ao clicar o botão e retorna true ou false
+        tarefaCompleta.classList.toggle('done');
+    }
+
+    // exportando o botaoConclui
+    export default botaoConclui
---
+++ main.js
@@ -0,0 +1,64 @@
+// chamando os componentes encapsulados
+
+
+import botaoConclui from "./componentes/concluiTarefa.js";
+import BotaoDeleta from "./componentes/deletaTarefas.js";
+
+const criarTarefa = (evento) => {
+
+        //ele previne o comportamento padrão e deixa salvo no console do navegador, ou seja, não teve refresh.
+        evento.preventDefault()
+
+        //acessa a ul e o form do html armazenando nas variáveis
+        const lista = document.querySelector('[data-list]');
+        const input = document.querySelector('[data-form-input]');
+
+        //pega o valor atribuído ao input do form
+        const valor = input.value;
+
+        //método que cria elementos e armazena em tarefa
+        const tarefa = document.createElement('li');
+
+        //A tarefa/li recebe a classe 'task' através do ClassList e adiciona. 
+        tarefa.classList.add('task');
+        
+        //template de conteúdo que irá ser adicionado ao tarefa/li. 
+        const conteudo = `<p class="content">${valor}</p>`;
+
+        //recebe o parágrafo de conteudo 
+        tarefa.innerHTML = conteudo;
+
+        //cria um elemento filho da li
+        tarefa.appendChild(botaoConclui())
+        tarefa.appendChild(BotaoDeleta())
+        //cria um filho da ul ou lista/[data-list]
+        lista.appendChild(tarefa);
+
+        //aqui ele limpa o input
+        input.value = " ";  
+
+
+    }
+
+    /* Verifica se o input está vazio */
+    const verificaVazio = (evento) => {
+        evento.preventDefault();
+    
+        const input = document.querySelector('[data-form-input]');
+        const valor = input.value;
+        const valorComparacao = valor.trim();
+    
+        if(valorComparacao === ""){
+            alert('O campo de texto está vázio! Digite alguma tarefa.')
+        } else {
+            criarTarefa(evento);
+        }
+    }
+
+    // responsável por acessar o button do html 
+    const novaTarefa = document.querySelector('[data-form-button]');
+
+    //através do click com o método/evento no button, a função criarTarefa
+    novaTarefa.addEventListener('click', verificaVazio);
+
+
